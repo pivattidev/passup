a77c4aa9a8a60ce4499ddd3c0a5a9081c259b5ec
pivattidev <alepivatti@gmail.com>
2026-09-12 22:53:03 -0300

    feat: Página de cadastro feita

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
+import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Cabecalho from './components/Cabecalho'
 import Rodape from './components/Rodape'
+import type { ContextoAplicacao, Usuario } from './types/Usuario'
 
 export default function App() {
+  const [usuarioCadastrado, setUsuarioCadastrado] = useState<Usuario | null>(null)
+
+  function cadastrarUsuario(usuario: Usuario) {
+    setUsuarioCadastrado(usuario)
+  }
+
+  const contextoAplicacao: ContextoAplicacao = {
+    usuarioCadastrado,
+    cadastrarUsuario,
+  }
+
   return (
     <div className="flex min-h-screen flex-col">
       <Cabecalho />
 
       <div className="flex-1">
-        <Outlet />
+        <Outlet context={contextoAplicacao} />
       </div>
 
       <Rodape />
